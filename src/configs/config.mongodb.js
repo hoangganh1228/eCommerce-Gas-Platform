876de1938856f73579cab36fb3a038aca4afc3b6
Hoàng Anh. Nguyễn <hoanganh52521352@gmail.com>
2025-01-21 10:46:50 +0700

    Section 4: Lịch sử của .env và cách kết hợp env và configs cho dự án nhiều môi trường và Members

---
+++ src/configs/config.mongodb.js
@@ -0,0 +1,42 @@
+'use strict'
+
+// lv0
+// const config = {
+//   app: {
+//     port: 3000
+//   },
+//   db: {
+//     host: 'localhost',
+//     port: 27017,
+//     name: 'db'
+//   }
+// }
+
+const dev = {
+  app: {
+    port: 3000
+  },
+  db: {
+    host: 'localhost',
+    port: 27017,
+    name: 'dbDev'
+  }
+}
+
+const pro = {
+  app: {
+    port: 3000
+  },
+  db: {
+    host: 'localhost',
+    port: 27017,
+    name: 'dbProduct'
+  }
+}
+
+const config = { dev, pro }
+const env = process.env.NODE_ENV || 'dev'
+
+// console.log(config[env], env);   
+
+module.exports = config[env]
