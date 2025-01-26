bf523c40ece1d3d8d8aa6fab8a2a814f54311e2f
Hoàng Anh. Nguyễn <hoanganh52521352@gmail.com>
2025-01-26 14:27:01 +0700

    Section 15: Thiết kế Api Service Product kết hợp giữa Factory và Strategy quá hiệu quả (Video Bonus)

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -2,13 +2,22 @@
 const { SuccessResponse } = require("../../core/success.response");
 const { product } = require("../models/product.model");
 const ProductService = require("../services/product.service")
+const ProductServiceV2 = require("../services/product.service.xxx")
 
 class ProductController {
 
   createProduct = async (req, res, next) => {
+    // new SuccessResponse({
+    //   message: 'Create new product success',
+    //   metadata: await ProductService.createProduct(req.body.product_type, {
+    //     ...req.body,
+    //     product_shop: req.user.userId
+    //   })
+    // }).send(res)
+
     new SuccessResponse({
       message: 'Create new product success',
-      metadata: await ProductService.createProduct(req.body.product_type, {
+      metadata: await ProductServiceV2.createProduct(req.body.product_type, {
         ...req.body,
         product_shop: req.user.userId
       })
--- src/models/product.model.js
+++ src/models/product.model.js
@@ -30,10 +30,7 @@ var productSchema = new Schema({
     type:String,
     enum: ['Electronics', 'Clothing', 'Furniture']
   },
-  product_shop: {
-    type: Schema.Types.ObjectId,
-    ref: 'Shop'
-  },
+  
   product_attributes: {
     type: Schema.Types.Mixed,
     required: true
@@ -51,7 +48,11 @@ const clothingSchema = new Schema({
     required: true
   },
   size: String,
-  material: String
+  material: String,
+  product_shop: {
+    type: Schema.Types.ObjectId,
+    ref: 'Shop'
+  },
 }, {
   collection: 'clothes',
   timestamps: true
@@ -65,9 +66,30 @@ const electronicSchema = new Schema({
     required: true
   },
   model: String,
-  color: String
+  color: String,
+  product_shop: {
+    type: Schema.Types.ObjectId,
+    ref: 'Shop'
+  },
 }, {
   collection: 'electronics',
+  timestamps: true
+})
+
+// define the product type = furniture
+const furnitureSchema = new Schema({
+  brand: {
+    type: String, 
+    required: true
+  },
+  size: String,
+  material: String,
+  product_shop: {
+    type: Schema.Types.ObjectId,
+    ref: 'Shop'
+  },
+}, {
+  collection: 'furnitures',
   timestamps: true
 })
 
@@ -75,5 +97,6 @@ const electronicSchema = new Schema({
 module.exports = {
   product: model(DOCUMENT_NAME, productSchema),
   electronic: model('Electronics', electronicSchema),
-  clothing: model('Clothing', clothingSchema)
+  clothing: model('Clothing', clothingSchema),
+  furniture: model('Furniture', furnitureSchema)
 };
